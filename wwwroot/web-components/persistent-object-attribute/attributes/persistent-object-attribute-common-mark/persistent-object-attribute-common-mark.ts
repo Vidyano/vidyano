7725d31fce20460629e4b3b0bd77793203ea7d27
David Sleeckx <david@sleeckx.be>
2021-11-10 17:50:36 +0100

    WIP - Components: CommonMark

---
+++ wwwroot/web-components/persistent-object-attribute/attributes/persistent-object-attribute-common-mark/persistent-object-attribute-common-mark.ts
@@ -0,0 +1,15 @@
+import * as Polymer from '../../../../libs/@polymer/polymer.js';
+import * as Vidyano from "../../../../libs/vidyano/vidyano.js"
+import { WebComponent } from "../../../web-component/web-component.js"
+import  { PersistentObjectAttribute } from "../../persistent-object-attribute.js"
+import "@polymer/marked-element/marked-element.js"
+
+@WebComponent.register()
+export class PersistentObjectAttributeCommonMark extends PersistentObjectAttribute {
+    static get template() { return Polymer.html`<link rel="import" href="persistent-object-attribute-common-mark.html">`; }
+
+    private _editTextAreaBlur() {
+        if (this.attribute && this.attribute.isValueChanged && this.attribute.triggersRefresh)
+            this.attribute.setValue(this.value = this.attribute.value, true).catch(Vidyano.noop);
+    }
+}
