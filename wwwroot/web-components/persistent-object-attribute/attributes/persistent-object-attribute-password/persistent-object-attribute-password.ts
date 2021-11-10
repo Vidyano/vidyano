cb4ea08a569dd84b619128f41fb448a53c7f9255
David Sleeckx <david@sleeckx.be>
2021-11-10 16:01:43 +0100

    WIP - Components: Password

---
+++ wwwroot/web-components/persistent-object-attribute/attributes/persistent-object-attribute-password/persistent-object-attribute-password.ts
@@ -0,0 +1,14 @@
+import * as Polymer from '../../../../libs/@polymer/polymer.js';
+import * as Vidyano from "../../../../libs/vidyano/vidyano.js"
+import { WebComponent } from "../../../web-component/web-component.js"
+import { PersistentObjectAttribute } from "../../persistent-object-attribute.js"
+
+@WebComponent.register()
+export class PersistentObjectAttributePassword extends PersistentObjectAttribute {
+    static get template() { return Polymer.html`<link rel="import" href="persistent-object-attribute-password.html">`; }
+
+    private _editInputBlur() {
+        if (this.attribute && this.attribute.isValueChanged && this.attribute.triggersRefresh)
+            this.attribute.setValue(this.value = this.attribute.value, true).catch(Vidyano.noop);
+    }
+}
