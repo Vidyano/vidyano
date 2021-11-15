83aa9a5d5fe1d4c8e628866f7c30c9ec16e18d9a
David Sleeckx <david@sleeckx.be>
2021-11-15 10:48:28 +0100

    WIP - Components: Boolean/NullableBoolean

---
+++ wwwroot/web-components/persistent-object-attribute/attributes/persistent-object-attribute-boolean/persistent-object-attribute-boolean.ts
@@ -0,0 +1,52 @@
+import * as Polymer from '../../../../libs/@polymer/polymer.js';
+import * as Vidyano from "../../../../libs/vidyano/vidyano.js"
+import { WebComponent } from "../../../web-component/web-component.js"
+import { PersistentObjectAttribute } from "../../persistent-object-attribute.js"
+
+@WebComponent.register({
+    properties: {
+        canToggle: {
+            type: Boolean,
+            computed: "_computeCanToggle(editing, readOnly)"
+        },
+        defaultInputtype: {
+            type: String,
+            readOnly: true
+        },
+        isCheckbox: {
+            type: Boolean,
+            computed: "_computeIsCheckbox(attribute, defaultInputtype)"
+        }
+    }
+})
+export class PersistentObjectAttributeBoolean extends PersistentObjectAttribute {
+    static get template() { return Polymer.html`<link rel="import" href="persistent-object-attribute-boolean.html">`; }
+
+    readonly defaultInputtype: string; private _setDefaultInputtype: (defaultInputtype: string) => void;
+
+    connectedCallback() {
+        super.connectedCallback();
+
+        this._setDefaultInputtype(this.app.configuration.getSetting("vi-persistent-object-attribute-boolean.inputtype", "toggle").toLowerCase());
+    }
+
+    protected _valueChanged(newValue: any) {
+        if (this.attribute && newValue !== this.attribute.value)
+            this.attribute.setValue(newValue, true).catch(Vidyano.noop);
+    }
+
+    private _computeCanToggle(editing: boolean, isReadOnly: boolean): boolean {
+        return editing && !isReadOnly;
+    }
+
+    private _computeIsDisabled(isReadOnly: boolean, isFrozen: boolean): boolean {
+        return isReadOnly || isFrozen;
+    }
+
+    private _computeIsCheckbox(attribute: Vidyano.PersistentObjectAttribute, defaultInputtype: string): boolean {
+        const checkbox = attribute.getTypeHint("inputtype", defaultInputtype, undefined, true) === "checkbox";
+        import(checkbox ? "../../../checkbox/checkbox.js" : "../../../toggle/toggle.js")
+
+        return checkbox;
+    }
+}
