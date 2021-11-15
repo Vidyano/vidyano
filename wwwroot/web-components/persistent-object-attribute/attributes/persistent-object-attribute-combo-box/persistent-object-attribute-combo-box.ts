9ba6a340e8a479c4a136a5386f5c6205a7acd951
David Sleeckx <david@sleeckx.be>
2021-11-15 13:37:38 +0100

    WIP - Components: ComboBox

---
+++ wwwroot/web-components/persistent-object-attribute/attributes/persistent-object-attribute-combo-box/persistent-object-attribute-combo-box.ts
@@ -0,0 +1,71 @@
+import * as Polymer from '../../../../libs/@polymer/polymer.js';
+import * as Vidyano from "../../../../libs/vidyano/vidyano.js"
+import { WebComponent } from "../../../web-component/web-component.js"
+import { PersistentObjectAttribute } from "../../persistent-object-attribute.js"
+import { Select } from '../../../select/select.js'
+import "../../../select/select.js"
+
+@WebComponent.register({
+    properties: {
+        newValue: {
+            type: String,
+            value: null,
+            notify: true
+        },
+        comboBoxOptions: {
+            type: Array,
+            readOnly: true
+        },
+        canAdd: {
+            type: Boolean,
+            computed: "_computeCanAdd(newValue, comboBoxOptions)"
+        }
+    }
+})
+export class PersistentObjectAttributeComboBox extends PersistentObjectAttribute {
+    static get template() { return Polymer.html`<link rel="import" href="persistent-object-attribute-combo-box.html">`; }
+
+    readonly comboBoxOptions: string[]; private _setComboBoxOptions: (options: string[]) => void;
+    newValue: string;
+
+    protected _editingChanged() {
+        super._editingChanged();
+
+        if (this.newValue) {
+            this.newValue = null;
+            this._optionsChanged();
+        }
+    }
+
+    protected _valueChanged(newValue: any) {
+        if (this.attribute && newValue !== this.attribute.value)
+            this.attribute.setValue(newValue, true).catch(Vidyano.noop);
+    }
+
+    protected _optionsChanged() {
+        const options = this.attribute.options ? (<string[]>this.attribute.options).slice() : [];
+
+        let empty = options.indexOf(null);
+        if (empty < 0)
+            empty = options.indexOf("");
+
+        if (options.indexOf(this.attribute.value) < 0) {
+            options.splice(empty >= 0 ? empty + 1 : 0, 0, this.attribute.value);
+        }
+
+        this._setComboBoxOptions(options);
+    }
+
+    private _add() {
+        this.value = this.newValue;
+        this._optionsChanged();
+    }
+
+    private _computeCanAdd(newValue: string, options: string[]): boolean {
+        return newValue != null && options && !options.some(o => o === newValue);
+    }
+
+    protected _onFocus(e: FocusEvent) {
+        this._focusElement(<Select>this.shadowRoot.querySelector("vi-select"));
+    }
+}
