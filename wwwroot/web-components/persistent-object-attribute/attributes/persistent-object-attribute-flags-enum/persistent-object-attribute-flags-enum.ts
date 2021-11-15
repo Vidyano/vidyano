3be5edfe82e03d53d62d24fe04a617052b613951
David Sleeckx <david@sleeckx.be>
2021-11-15 14:50:09 +0100

    WIP - Components: PersistentObjectAttributeFlagsEnum

---
+++ wwwroot/web-components/persistent-object-attribute/attributes/persistent-object-attribute-flags-enum/persistent-object-attribute-flags-enum.ts
@@ -0,0 +1,9 @@
+import * as Polymer from '../../../../libs/@polymer/polymer.js';
+import { WebComponent } from "../../../web-component/web-component.js"
+import { PersistentObjectAttribute } from "../../persistent-object-attribute.js"
+import "./persistent-object-attribute-flags-enum-flag.js"
+
+@WebComponent.register()
+export class PersistentObjectAttributeFlagsEnum extends PersistentObjectAttribute {
+    static get template() { return Polymer.html`<link rel="import" href="persistent-object-attribute-flags-enum.html">`; }
+}
