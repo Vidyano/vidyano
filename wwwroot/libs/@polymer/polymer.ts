dc57aa0d8003ac98c362e51de242151ae8905e9c
David Sleeckx <david@sleeckx.be>
2021-11-17 13:07:42 +0100

    Fixed missing import

--- wwwroot/libs/@polymer/polymer.ts
+++ wwwroot/libs/@polymer/polymer.ts
@@ -14,6 +14,7 @@ export * as Url from "@polymer/polymer/lib/utils/resolve-url.js"
 export * as Templatize from "@polymer/polymer/lib/utils/templatize.js"
 export * from "@polymer/polymer/lib/mixins/gesture-event-listeners.js"
 export * from "@polymer/polymer/lib/utils/mixin.js"
+export * from "@polymer/iron-overlay-behavior/iron-focusables-helper.js"
 import "@polymer/iron-media-query/iron-media-query.js"
 
 declare module "@polymer/polymer/lib/utils/gestures.js" {
