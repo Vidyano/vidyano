ba032c8a209005ba530392dd5ddf1a055e68874d
David Sleeckx <david@sleeckx.be>
2021-11-22 12:01:00 +0100

    Added query grid column header sorting

--- wwwroot/web-components/query-grid/query-grid-column-header.ts
+++ wwwroot/web-components/query-grid/query-grid-column-header.ts
@@ -29,23 +29,50 @@ resizeObserver = new ResizeObserver(entries => {
 
 @WebComponent.register({
     properties: {
-        column: Object,
+        column: {
+            type: Object,
+            observer: "_columnChanged"
+        },
+        canSort: {
+            type: Boolean,
+            reflectToAttribute: true,
+            readOnly: true
+        },
         sortingIcon: {
             type: String,
             computed: "_computeSortingIcon(column.sortDirection)"
+        },
+        canGroupBy: {
+            type: Boolean,
+            readOnly: true
+        },
+        isPinned: {
+            type: Boolean,
+            readOnly: true
+        },
+        groupByLabel: {
+            type: String,
+            computed: "_computeGroupByLabel(column.label, translations)"
+        },
+        pinLabel: {
+            type: String,
+            computed: "_computePinLabel(isPinned, translations)"
         }
     },
     forwardObservers: [
         "column.sortDirection"
     ],
     listeners: {
-        
+        "tap": "_sort"
     }
 })
 export class QueryGridColumnHeader extends WebComponentListener(WebComponent) {
     static get template() { return Polymer.html`<link rel="import" href="query-grid-column-header.html">` }
 
     column: Vidyano.QueryColumn;
+    readonly canSort: boolean; private _setCanSort: (canSort: boolean) => void;
+    readonly canGroupBy: boolean; private _setCanGroupBy: (canGroupBy: boolean) => void;
+    readonly isPinned: boolean; private _setIsPinned: (isPinned: boolean) => void;
 
     connectedCallback() {
         super.connectedCallback();
@@ -59,7 +86,69 @@ export class QueryGridColumnHeader extends WebComponentListener(WebComponent) {
         resizeObserver.unobserve(this);
     }
 
+    private _columnChanged(column: Vidyano.QueryColumn) {
+        if (!column)
+            return;
+        
+        this._setCanSort(column.canSort);
+        this._setCanGroupBy(column.canGroupBy);
+        this._setIsPinned(column.isPinned);
+    }
+
     private _computeSortingIcon(direction: Vidyano.SortDirection) {
         return direction === "ASC" ? "SortAsc" : (direction === "DESC" ? "SortDesc" : null);
+    }
+
+    private _computeGroupByLabel(label: string): string {
+        return this.translateMessage("GroupByColumn", label);
+    }
+
+    private _computePinLabel(isPinned: boolean): string {
+        return isPinned ? this.translations.Unpin : this.translations.Pin;
+    }
+
+    private _sort(direction: Vidyano.SortDirection);
+    private _sort(e: Polymer.Gestures.TapEvent);
+    private _sort(eventOrDirection: Vidyano.SortDirection | Polymer.Gestures.TapEvent) {
+        let newSortingDirection: Vidyano.SortDirection;
+        let multiSort = false;
+
+        if (typeof eventOrDirection === "string")
+            newSortingDirection = eventOrDirection;
+        else {
+            multiSort = (<any>eventOrDirection).detail.sourceEvent.ctrlKey;
+            switch (this.column.sortDirection) {
+                case "ASC": {
+                    newSortingDirection = "DESC";
+                    break;
+                }
+                case "DESC": {
+                    newSortingDirection = multiSort && this.column.query.sortOptions.length > 1 ? "" : "ASC";
+                    break;
+                }
+                case "": {
+                    newSortingDirection = "ASC";
+                    break;
+                }
+            }
+        }
+
+        this.column.sort(newSortingDirection, multiSort);
+    }
+
+    private _sortAsc(e: Polymer.Gestures.TapEvent) {
+        e.stopPropagation();
+        this._sort("ASC");
+    }
+
+    private _sortDesc(e: Polymer.Gestures.TapEvent) {
+        e.stopPropagation();
+        this._sort("DESC");
+    }
+
+    private _togglePin() {
+        // TODO: Fire event to update grid with optional argument to reset horizontal offset to 0
+        this.column.isPinned = !this.column.isPinned;
+        this._setIsPinned(this.column.isPinned);
     }
 }
