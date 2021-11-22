0bf70ca060364219539da22d959931e91da36718
David Sleeckx <david@sleeckx.be>
2021-11-22 16:20:24 +0100

    Added list component

---
+++ wwwroot/test/list-test/list-test.ts
@@ -0,0 +1,17 @@
+import * as Polymer from "../../libs/@polymer/polymer.js"
+import { WebComponent, WebComponentListener } from "../../web-components/web-component/web-component.js"
+import "../../web-components/list/list.js"
+
+@WebComponent.register({
+    properties: {
+        items: {
+            type: Array,
+            value: () => {
+                return Array.range(1, 10000)
+            }
+        }
+    }
+})
+export class ListTest extends WebComponentListener(WebComponent) {
+    static get template() { return Polymer.html`<link rel="import" href="list-test.html">`; }
+}
---
+++ wwwroot/web-components/list/list.ts
@@ -0,0 +1,55 @@
+import "@polymer/iron-list"
+import * as Polymer from "../../libs/@polymer/polymer.js"
+import { Scroller } from "../scroller/scroller.js"
+import { WebComponent } from "../web-component/web-component.js"
+
+@WebComponent.register({
+    properties: {
+        items: Array,
+        as: String,
+        parentScroller: {
+            type: Boolean,
+            reflectToAttribute: true,
+            value: false
+        }
+    },
+    observers: [
+        "_hookIronListToScroller(parentScroller, isConnected)"
+    ]
+})
+export class List extends WebComponent {
+    static get template() { return Polymer.html`<link rel="import" href="list.html">` }
+
+    parentScroller: boolean;
+
+    private _hookIronListToScroller(parentScroller: boolean, isConnected: boolean) {
+        if (!isConnected)
+            return;
+
+        Polymer.flush();
+        Polymer.Async.microTask.run(() => {
+            if (parentScroller !== this.parentScroller || !this.isConnected)
+                return;
+
+            const list = this.shadowRoot.querySelector("#list");
+            const scroller = <any>this.findParent(e => e instanceof Scroller, list);
+            (<any>list).scrollTarget = scroller.$.wrapper;
+
+            this._sizeChanged();
+        });
+    }
+
+    private _bindIronListDataHost() {
+        // Workaround for making sure events are delegated to the correct host
+        const list = this.shadowRoot.querySelector("#list");
+        if (list["dataHost"] && list["dataHost"]["_rootDataHost"] === this) {
+            const dataHostParent = this.findParent(e => e["dataHost"]);
+            if (dataHostParent)
+                list["dataHost"] = dataHostParent["dataHost"];
+        }
+    }
+
+    private _sizeChanged() {
+        this.shadowRoot.querySelector("#list").dispatchEvent(new CustomEvent("iron-resize"));
+    }
+}
