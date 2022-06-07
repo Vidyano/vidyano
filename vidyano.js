c1535651b097b733410071c6dbf268d3271851a1
David Sleeckx <david@sleeckx.be>
2022-06-07 17:24:03 +0200

    Bump version

--- vidyano.js
+++ vidyano.js
@@ -35323,7 +35323,7 @@ class WebComponent extends GestureEventListeners(PolymerElement) {
                 if (userStyleModule != null) {
                     const style = document.createElement("style");
                     style.setAttribute("include", `${elementName}-style-module`);
-                    const lastStyle = Array.from(template.content.querySelectorAll(":scope style")).filter(s => s.parentNode === template.content).pop();
+                    const lastStyle = Array.from(template.content.children).filter(c => c instanceof HTMLStyleElement).pop();
                     if (lastStyle != null)
                         template.content.insertBefore(style, lastStyle.nextSibling);
                     else
