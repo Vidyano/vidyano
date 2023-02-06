3f79b3089c9a386a7423cf057f2153ffd0d93e63
David Sleeckx <david@sleeckx.be>
2023-02-06 17:20:41 +0100

    Fixed typescript overload issue

--- wwwroot/libs/vidyano/service-object-with-actions.ts
+++ wwwroot/libs/vidyano/service-object-with-actions.ts
@@ -47,7 +47,7 @@ export class ServiceObjectWithActions extends ServiceObject {
 
     setNotification(notification?: string, type?: NotificationType, duration?: number, skipShowNotification?: boolean);
     setNotification(notification?: Error, type?: NotificationType, duration?: number, skipShowNotification?: boolean);
-    setNotification(notificationOrError: string | Error, type: NotificationType = "Error", duration: number = null, skipShowNotification?: boolean) {
+    setNotification(notificationOrError: string | Error = null, type: NotificationType = "Error", duration: number = null, skipShowNotification?: boolean) {
         const notification = typeof notificationOrError === "string" || !notificationOrError ? notificationOrError as string : notificationOrError?.["message"];
 
         const oldNotificationDuration = this.notificationDuration;
