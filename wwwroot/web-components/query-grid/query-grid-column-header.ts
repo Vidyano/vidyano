32d621691a878aef7b5d64c9e503568f79a93c59
Frederic de Bakker <frederic@2sky.be>
2023-08-29 11:58:50 +0200

    Fix formatting 3

--- wwwroot/web-components/query-grid/query-grid-column-header.ts
+++ wwwroot/web-components/query-grid/query-grid-column-header.ts
@@ -10,7 +10,7 @@ let resizeObserver: ResizeObserver;
 resizeObserver = new ResizeObserver(allEntries => {
     window.requestAnimationFrame(() => {
         // Entries may be batched for multiple grids, make sure the event is dispatched to the correct grid
-            
+        
         const parents = new Map<HTMLElement, ResizeObserverEntry[]>();
         allEntries.forEach(e => {
             const parent = parents.get(e.target.parentElement) || parents.set(e.target.parentElement, []).get(e.target.parentElement);
@@ -111,7 +111,7 @@ export class QueryGridColumnHeader extends WebComponent {
     private _renderPopupMenu(e: Event) {
         e.stopPropagation();
 
-        this._setRenderPopupMenu(true);     
+        this._setRenderPopupMenu(true);
         Polymer.flush();
 
         const menu = <PopupMenu>this.shadowRoot.querySelector("#menu");
@@ -120,8 +120,8 @@ export class QueryGridColumnHeader extends WebComponent {
 
     private _columnChanged(column: QueryGridColumn, oldColumn: QueryGridColumn) {
         if (!column)
-            return;     
-
+            return;
+        
         this._setCanSort(column.canSort);
         this._setCanGroupBy(column.canGroupBy);
         this._setIsPinned(column.isPinned);
