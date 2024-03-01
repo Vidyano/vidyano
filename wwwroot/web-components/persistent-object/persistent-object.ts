89adef13fe1f724dfa94a271ebf3864d0ed5e2f1
Thierry Rosseau <50580460+ThierryRosseau@users.noreply.github.com>
2024-03-01 12:28:06 +0100

    Update wwwroot/web-components/persistent-object/persistent-object.ts
    Co-authored-by: David <david@sleeckx.be>

--- wwwroot/web-components/persistent-object/persistent-object.ts
+++ wwwroot/web-components/persistent-object/persistent-object.ts
@@ -257,7 +257,7 @@ export class PersistentObject extends WebComponent implements IPersistentObjectW
     }
 
     private _computeShowNavigation(persistentObject: Vidyano.PersistentObject) {
-        return !persistentObject.isNew && !!persistentObject.ownerQuery && !!persistentObject.ownerQuery.totalItems && !persistentObject.isBulkEdit;
+        return !persistentObject.isNew && persistentObject.ownerQuery?.totalItems > 0 && !persistentObject.isBulkEdit;
     }
 
     private _tabselect(e: CustomEvent) {
