ac89ea3fdf444a9fb3ff3883adcfdf087e42f82f
David Sleeckx <david@sleeckx.be>
2025-02-12 11:03:23 +0100

    Open common mark links in new tab when href starts with http
    Updated marked package

--- wwwroot/web-components/marked/marked.ts
+++ wwwroot/web-components/marked/marked.ts
@@ -3,6 +3,14 @@ import { WebComponent } from "../web-component/web-component.js"
 import { marked } from "marked"
 import DOMPurify from "dompurify"
 
+DOMPurify.addHook("afterSanitizeAttributes", function (node) {
+    // set all elements owning target to target=_blank
+    if ("target" in node) {
+        node.setAttribute("target", "_blank");
+        node.setAttribute("rel", "noopener");
+    }
+});
+
 @WebComponent.register({
     properties: {
         addTags: {
@@ -31,38 +39,25 @@ export class Marked extends WebComponent {
     static get template() { return Polymer.html`<link rel="import" href="marked.html">` }
 
     private async _markdownChanged(markdown: string, breaks: boolean, gfm: boolean, addTags: string, forbidTags: string) {
-        this.innerHTML = await marked(markdown, {
+        var renderer = new marked.Renderer();
+        renderer.link = function(token) {
+            var anchor = marked.Renderer.prototype.link.call(this, token) as string;
+            if (token.href.startsWith("http"))
+                return anchor.replace("<a",`<a target="_blank" rel="noopener" `);
+
+            return anchor;
+        };
+
+        const html = await marked(markdown, {
             breaks,
             gfm,
             async: false,
-            hooks: {
-                preprocess: (markdown: string) => (markdown),
-                postprocess: (html: string) => DOMPurify.sanitize(html, {
-                    ADD_TAGS: addTags?.split(",") || [],
-                    FORBID_TAGS: forbidTags?.split(",") || []
-                })
-            }
+            renderer: renderer
         });
-    }
-}
 
-// TODO: Remove element by October 1st 2024
-@WebComponent.register({
-    properties: {
-        breaks: {
-            type: Boolean,
-            value: true
-        },
-        markdown: String
-    },
-    observers: [
-        "_markdownChanged(markdown, breaks)"
-    ]
-}, "marked")
-class Element extends Marked {
-    connectedCallback() {
-        super.connectedCallback();
-
-        console.warn("marked-element is deprecated and set to be removed by October 1st 2024. Use vi-marked instead.");
+        this.innerHTML = DOMPurify.sanitize(html, {
+            ADD_TAGS: addTags?.split(",") || [],
+            FORBID_TAGS: forbidTags?.split(",") || [],
+        });
     }
 }
