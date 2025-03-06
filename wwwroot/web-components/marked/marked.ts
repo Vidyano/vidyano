ea4375eb06151674f255a3d1462d66d2df20bc56
David Sleeckx <david@sleeckx.be>
2025-03-06 16:26:10 +0100

    fix: Only add target=_blank for external links

--- wwwroot/web-components/marked/marked.ts
+++ wwwroot/web-components/marked/marked.ts
@@ -5,7 +5,7 @@ import DOMPurify from "dompurify"
 
 DOMPurify.addHook("afterSanitizeAttributes", function (node) {
     // set all elements owning target to target=_blank
-    if ("target" in node) {
+    if (node instanceof HTMLAnchorElement && node.getAttribute("href")?.startsWith("http")) {
         node.setAttribute("target", "_blank");
         node.setAttribute("rel", "noopener");
     }
@@ -54,24 +54,16 @@ export function getMarkdown(markdown: string, options?: {
     addTags?: string;
     forbidTags?: string;
 }) {
-    var renderer = new marked.Renderer();
-    renderer.link = function(token) {
-        var anchor = marked.Renderer.prototype.link.call(this, token) as string;
-        if (token.href.startsWith("http"))
-            return anchor.replace("<a",`<a target="_blank" rel="noopener" `);
-
-        return anchor;
-    };
-
     const html = marked(markdown, {
         breaks: options?.breaks !== false ? true : false,
         gfm: options?.gfm !== false ? true : false,
-        async: false,
-        renderer: renderer
+        async: false
     });
 
-    return DOMPurify.sanitize(html, {
+    const sanitized = DOMPurify.sanitize(html, {
         ADD_TAGS: options?.addTags?.split(",") || [],
-        FORBID_TAGS: options?.forbidTags?.split(",") || [],
+        FORBID_TAGS: options?.forbidTags?.split(",") || []
     });
+
+    return sanitized;
 }
