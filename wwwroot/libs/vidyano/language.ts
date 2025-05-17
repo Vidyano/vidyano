fdaa891a1a1fdd7a0d9b164a0cb8e8dee2c80296
David Sleeckx <david@sleeckx.be>
2025-05-17 13:12:55 +0200

    Language

--- wwwroot/libs/vidyano/language.ts
+++ wwwroot/libs/vidyano/language.ts
@@ -1,30 +1,56 @@
-import type * as Dto from "./typings/service.js"
-import { Observable } from "./common/observable.js"
-import type { ServiceObject } from "./service-object.js"
+import type * as Dto from "./typings/service.js";
+import { Observable } from "./common/observable.js";
+import type { ServiceObject } from "./service-object.js";
 
+/**
+ * Represents a language with culture, name, default status, and messages.
+ */
 export class Language extends Observable<ServiceObject> implements Dto.Language {
-    constructor(private _language: Dto.Language, private _culture: string) {
+    #language: Dto.Language;
+    #culture: string;
+
+    /**
+     * Initializes a new instance of the Language class.
+     * @param language The language DTO.
+     * @param culture The culture string.
+     */
+    constructor(language: Dto.Language, culture: string) {
         super();
+
+        this.#language = language;
+        this.#culture = culture;
     }
 
+    /**
+     * Gets the culture of the language.
+     */
     get culture(): string {
-        return this._culture;
+        return this.#culture;
     }
 
+    /**
+     * Gets the name of the language.
+     */
     get name(): string {
-        return this._language.name;
+        return this.#language.name;
     }
 
+    /**
+     * Gets whether this language is the default.
+     */
     get isDefault(): boolean {
-        return this._language.isDefault;
+        return this.#language.isDefault;
     }
 
+    /**
+     * Gets or sets the messages for this language.
+     */
     get messages(): { [key: string]: string; } {
-        return this._language.messages;
+        return this.#language.messages;
     }
 
     set messages(value: { [key: string]: string; }) {
-        const oldMessages = this._language.messages;
-        this.notifyPropertyChanged("messages", this._language.messages = value, oldMessages);
+        const oldMessages = this.#language.messages;
+        this.notifyPropertyChanged("messages", (this.#language.messages = value), oldMessages);
     }
 }
