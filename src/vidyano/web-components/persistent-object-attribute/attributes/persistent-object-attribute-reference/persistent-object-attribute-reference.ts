0a4001acbb3a304077bf76c4450c3288943747ec
David Sleeckx <david@sleeckx.be>
2025-07-29 20:49:02 +0200

    chore: Code cleanup

--- src/vidyano/web-components/persistent-object-attribute/attributes/persistent-object-attribute-reference/persistent-object-attribute-reference.ts
+++ src/vidyano/web-components/persistent-object-attribute/attributes/persistent-object-attribute-reference/persistent-object-attribute-reference.ts
@@ -212,11 +212,11 @@ export class PersistentObjectAttributeReference extends PersistentObjectAttribut
     }
 
     private _computeInputType(attribute: Vidyano.PersistentObjectAttributeWithReference): string {
-        return attribute && attribute.getTypeHint("inputtype", "default", undefined);
+        return attribute && attribute.getTypeHint("inputtype", "default");
     }
 
     private _computeOrientation(attribute: Vidyano.PersistentObjectAttributeWithReference): string {
-        return attribute && attribute.getTypeHint("orientation", "vertical", undefined);
+        return attribute && attribute.getTypeHint("orientation", "vertical");
     }
 
     private _computeCanOpenSelect(isReadOnly: boolean, options: string[]): boolean {
