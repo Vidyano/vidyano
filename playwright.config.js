770fb0302c2bcb32f4d9d6e2b068b57d2e6296cf
David Sleeckx <david@sleeckx.be>
2025-08-21 14:45:26 +0000

    test: Add Playwright test infrastructure and update dependencies

--- playwright.config.js
+++ playwright.config.js
@@ -21,9 +21,9 @@ export default defineConfig({
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
+  workers: 1, // Don't run tests in parallel since we are mostly running E2E tests that can interfere with each other.
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'null',
+  reporter: 'list',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
@@ -73,9 +73,10 @@ export default defineConfig({
 
   /* Run your local dev server before starting the tests */
   // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://localhost:3000',
+  //   command: 'npx http-server . -p 8888 -c-1',
+  //   url: 'http://localhost:8888',
   //   reuseExistingServer: !process.env.CI,
+  //   timeout: 30 * 1000,
   // },
 });
 
