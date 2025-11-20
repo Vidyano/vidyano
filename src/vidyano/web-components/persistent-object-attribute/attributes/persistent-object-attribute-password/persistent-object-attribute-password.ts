35b43183a080da1e97375f03be44eeca925720da
David Sleeckx <david@sleeckx.be>
2025-11-20 09:27:17 +0000

    refactor: Migrate vi-persistent-object-attribute-password from Polymer to Lit

--- src/vidyano/web-components/persistent-object-attribute/attributes/persistent-object-attribute-password/persistent-object-attribute-password.ts
+++ src/vidyano/web-components/persistent-object-attribute/attributes/persistent-object-attribute-password/persistent-object-attribute-password.ts
@@ -1,33 +1,56 @@
-import * as Polymer from "polymer"
-import * as Vidyano from "vidyano"
-import * as PersistentObjectAttributeRegister from "components/persistent-object-attribute/persistent-object-attribute-register"
+import { html, unsafeCSS } from "lit";
+import { state } from "lit/decorators.js";
+import * as Vidyano from "vidyano";
+import { PersistentObjectAttribute } from "components/persistent-object-attribute/persistent-object-attribute";
+import * as PersistentObjectAttributeRegister from "components/persistent-object-attribute/persistent-object-attribute-register";
+import styles from "./persistent-object-attribute-password.css";
 
-@Polymer.WebComponent.register({
-    properties: {
-        autocomplete: {
-            type: String,
-            readOnly: true
-        }
-    }
-}, "vi-persistent-object-attribute-password")
-export class PersistentObjectAttributePassword extends Polymer.PersistentObjectAttribute {
-    static get template() { return Polymer.html`<link rel="import" href="persistent-object-attribute-password.html">`; }
+export class PersistentObjectAttributePassword extends PersistentObjectAttribute {
+    static styles = [super.styles, unsafeCSS(styles)];
 
-    readonly autocomplete: string; private _setAutocomplete: (maxlength: string) => void;
+    @state()
+    private autocomplete: string;
 
     protected _attributeChanged() {
         super._attributeChanged();
 
-        if (this.attribute instanceof Vidyano.PersistentObjectAttribute) {
-            // Sets the autocomplete attribute on the input (https://developer.mozilla.org/en-US/docs/Web/HTML/Attributes/autocomplete)
-            this._setAutocomplete(this.attribute.getTypeHint("Autocomplete"));
+        if (this.attribute instanceof Vidyano.PersistentObjectAttribute)
+            this.autocomplete = this.attribute.getTypeHint("Autocomplete");
+    }
+
+    private async _editInputBlur() {
+        if (this.attribute?.isValueChanged && this.attribute.triggersRefresh) {
+            try {
+                await this.attribute.setValue(this.value = this.attribute.value, true);
+            }
+            catch (e) {
+                console.error(`Failed to set password value on blur for '${this.attribute.parent.fullTypeName}.${this.attribute.name}':`, e);
+            }
         }
     }
 
-    private _editInputBlur() {
-        if (this.attribute && this.attribute.isValueChanged && this.attribute.triggersRefresh)
-            this.attribute.setValue(this.value = this.attribute.value, true).catch(Vidyano.noop);
+    protected renderDisplay() {
+        return html`●●●●●●`;
+    }
+
+    protected renderEdit() {
+        return html`
+            <vi-persistent-object-attribute-edit .attribute=${this.attribute}>
+                <vi-sensitive ?disabled=${!this.sensitive}>
+                    <input type="password"
+                        .value=${this.value || ""}
+                        autocomplete=${this.autocomplete || ""}
+                        ?readonly=${this.readOnly}
+                        tabindex=${this.readOnlyTabIndex}
+                        placeholder=${this.placeholder || ""}
+                        @input=${(e: InputEvent) => this.value = (e.target as HTMLInputElement).value}
+                        @blur=${this._editInputBlur}>
+                </vi-sensitive>
+            </vi-persistent-object-attribute-edit>
+        `;
     }
 }
+
+customElements.define("vi-persistent-object-attribute-password", PersistentObjectAttributePassword);
 
 PersistentObjectAttributeRegister.add("Password", PersistentObjectAttributePassword);
