45570bfd5347f1500370cfa508769c87bcf7ec63
David Sleeckx <david@sleeckx.be>
2025-11-21 09:57:13 +0000

    refactor: Migrate vi-persistent-object-attribute-boolean from Polymer to Lit

--- src/vidyano/web-components/persistent-object-attribute/attributes/persistent-object-attribute-boolean/persistent-object-attribute-boolean.ts
+++ src/vidyano/web-components/persistent-object-attribute/attributes/persistent-object-attribute-boolean/persistent-object-attribute-boolean.ts
@@ -1,52 +1,66 @@
-import * as Polymer from "polymer"
-import * as Vidyano from "vidyano"
-import "components/checkbox/checkbox"
-import "components/toggle/toggle"
-import * as PersistentObjectAttributeRegister from "components/persistent-object-attribute/persistent-object-attribute-register"
+import { html, nothing, unsafeCSS } from "lit";
+import { property, state } from "lit/decorators.js";
+import * as Vidyano from "vidyano";
+import "components/checkbox/checkbox";
+import "components/toggle/toggle";
+import { computed, observer } from "components/web-component/web-component";
+import { PersistentObjectAttribute } from "components/persistent-object-attribute/persistent-object-attribute";
+import * as PersistentObjectAttributeRegister from "components/persistent-object-attribute/persistent-object-attribute-register";
+import styles from "./persistent-object-attribute-boolean.css";
 
-@Polymer.WebComponent.register({
-    properties: {
-        canToggle: {
-            type: Boolean,
-            computed: "_computeCanToggle(editing, readOnly)"
-        },
-        defaultInputtype: {
-            type: String,
-            readOnly: true
-        },
-        isCheckbox: {
-            type: Boolean,
-            computed: "_computeIsCheckbox(attribute, defaultInputtype)"
+export class PersistentObjectAttributeBoolean extends PersistentObjectAttribute {
+    static styles = [super.styles, unsafeCSS(styles)];
+
+    @state()
+    inputtype: string;
+
+    @property({ type: Boolean })
+    @computed(function(this: PersistentObjectAttributeBoolean, inputtype: string): boolean {
+        return inputtype === "checkbox";
+    }, "inputtype")
+    declare readonly isCheckbox: boolean;
+
+    protected _attributeChanged() {
+        super._attributeChanged();
+
+        if (this.attribute instanceof Vidyano.PersistentObjectAttribute) {
+            const defaultInputtype = this.app.configuration.getSetting("vi-persistent-object-attribute-boolean.inputtype", "toggle").toLowerCase();
+            this.inputtype = this.attribute.getTypeHint("inputtype", defaultInputtype, undefined);
         }
     }
-}, "vi-persistent-object-attribute-boolean")
-export class PersistentObjectAttributeBoolean extends Polymer.PersistentObjectAttribute {
-    static get template() { return Polymer.html`<link rel="import" href="persistent-object-attribute-boolean.html">`; }
 
-    readonly defaultInputtype: string; private _setDefaultInputtype: (defaultInputtype: string) => void;
-
-    connectedCallback() {
-        super.connectedCallback();
-
-        this._setDefaultInputtype(this.app.configuration.getSetting("vi-persistent-object-attribute-boolean.inputtype", "toggle").toLowerCase());
-    }
-
+    @observer("value")
     protected _valueChanged(newValue: any) {
         if (this.attribute && newValue !== this.attribute.value)
             this.attribute.setValue(newValue, true).catch(Vidyano.noop);
     }
 
-    private _computeCanToggle(editing: boolean, isReadOnly: boolean): boolean {
-        return editing && !isReadOnly;
+    protected renderDisplay() {
+        return super.renderDisplay(html`<span>${this.attribute?.displayValue}</span>`);
     }
 
-    private _computeIsDisabled(isReadOnly: boolean, isFrozen: boolean): boolean {
-        return isReadOnly || isFrozen;
-    }
+    protected renderEdit() {
+        if (this.sensitive)
+            return this.renderDisplay();
 
-    private _computeIsCheckbox(attribute: Vidyano.PersistentObjectAttribute, defaultInputtype: string): boolean {
-        return attribute.getTypeHint("inputtype", defaultInputtype, undefined) === "checkbox";
+        return this.isCheckbox ? html`
+            <vi-checkbox
+                .checked=${this.value}
+                @checked-changed=${(e: CustomEvent) => this.value = e.detail.value}
+                label=${this.attribute?.displayValue || nothing}
+                ?disabled=${this.readOnly || this.frozen}>
+            </vi-checkbox>
+        ` : html`
+            <vi-toggle
+                .toggled=${this.value}
+                @toggled-changed=${(e: CustomEvent) => this.value = e.detail.value}
+                label=${this.attribute?.displayValue || nothing}
+                ?disabled=${this.readOnly || this.frozen}>
+            </vi-toggle>
+        `;
     }
 }
+
+customElements.define("vi-persistent-object-attribute-boolean", PersistentObjectAttributeBoolean);
 
 PersistentObjectAttributeRegister.add("Boolean", PersistentObjectAttributeBoolean);
