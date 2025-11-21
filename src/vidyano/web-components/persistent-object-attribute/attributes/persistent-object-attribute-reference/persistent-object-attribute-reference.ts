833123ec7bd7ea1cc708cdda50cee593860f3200
David Sleeckx <david@sleeckx.be>
2025-11-21 09:45:18 +0000

    refactor: Migrate vi-persistent-object-attribute-reference from Polymer to Lit

--- src/vidyano/web-components/persistent-object-attribute/attributes/persistent-object-attribute-reference/persistent-object-attribute-reference.ts
+++ src/vidyano/web-components/persistent-object-attribute/attributes/persistent-object-attribute-reference/persistent-object-attribute-reference.ts
@@ -1,97 +1,116 @@
-import * as Polymer from "polymer"
-import * as Vidyano from "vidyano"
-import { Path } from "libs/pathjs/pathjs"
-import * as PersistentObjectAttributeRegister from "components/persistent-object-attribute/persistent-object-attribute-register"
-import { SelectReferenceDialog } from "components/select-reference-dialog/select-reference-dialog"
-import type { Select } from "components/select/select"
-import "components/select/select"
+import { html, nothing, unsafeCSS } from "lit";
+import { property } from "lit/decorators.js";
+import * as Vidyano from "vidyano";
+import { Path } from "libs/pathjs/pathjs";
+import { computed, notify, observer } from "components/web-component/web-component";
+import { PersistentObjectAttribute } from "components/persistent-object-attribute/persistent-object-attribute";
+import * as PersistentObjectAttributeRegister from "components/persistent-object-attribute/persistent-object-attribute-register";
+import { SelectReferenceDialog } from "components/select-reference-dialog/select-reference-dialog";
+import type { Select } from "components/select/select";
+import "components/select/select";
+import styles from "./persistent-object-attribute-reference.css";
 
-@Polymer.WebComponent.register({
-    properties: {
-        href: String,
-        canClear: {
-            type: Boolean,
-            readOnly: true
-        },
-        canAddNewReference: {
-            type: Boolean,
-            readOnly: true
-        },
-        canBrowseReference: {
-            type: Boolean,
-            readOnly: true
-        },
-        filter: {
-            type: String,
-            notify: true
-        },
-        inputtype: {
-            type: String,
-            computed: "_computeInputType(attribute)"
-        },
-        objectId: {
-            type: String,
-            observer: "_objectIdChanged"
-        },
-        selectInPlace: {
-            type: Boolean,
-            reflectToAttribute: true,
-            computed: "attribute.selectInPlace"
-        },
-        canOpenSelect: {
-            type: Boolean,
-            computed: "_computeCanOpenSelect(readOnly, options)"
-        },
-        orientation: {
-            type: String,
-            computed: "_computeOrientation(attribute)"
-        },
-        target: {
-            type: String,
-            computed: "_computeTarget(attribute, href)"
-        },
-        title: {
-            type: String,
-            computed: "_computeTitle(attribute.displayValue, sensitive)"
-        }
-    },
-    observers: [
-        "_update(attribute.isReadOnly, sensitive, isConnected)"
-    ]
-}, "vi-persistent-object-attribute-reference")
-export class PersistentObjectAttributeReference extends Polymer.PersistentObjectAttribute {
-    static get template() { return Polymer.html`<link rel="import" href="persistent-object-attribute-reference.html">`; }
+export class PersistentObjectAttributeReference extends PersistentObjectAttribute {
+    static styles = [super.styles, unsafeCSS(styles)];
 
-    readonly canClear: boolean; private _setCanClear: (val: boolean) => void;
-    readonly canAddNewReference: boolean; private _setCanAddNewReference: (val: boolean) => void;
-    readonly canBrowseReference: boolean; private _setCanBrowseReference: (val: boolean) => void;
-    objectId: string;
-    attribute: Vidyano.PersistentObjectAttributeWithReference;
-    href: string;
+    declare attribute: Vidyano.PersistentObjectAttributeWithReference;
+
+    @property({ type: String })
+    @notify()
     filter: string;
 
-    connectedCallback() {
-        super.connectedCallback();
-        this._update();
-    }
+    @property({ type: String })
+    @computed("attribute.objectId")
+    declare readonly objectId: string;
+
+    @property({ type: String })
+    @computed(function(this: PersistentObjectAttributeReference, attribute: Vidyano.PersistentObjectAttributeWithReference): string {
+        return attribute && attribute.getTypeHint("inputtype", "default");
+    }, "attribute")
+    declare readonly inputtype: string;
+
+    @property({ type: Boolean, reflect: true })
+    @computed("attribute.selectInPlace")
+    declare readonly selectInPlace: boolean;
+
+    @property({ type: Boolean })
+    @computed(function(this: PersistentObjectAttributeReference, readOnly: boolean, options: string[]): boolean {
+        return !readOnly && !!options && options.length > 0;
+    }, "readOnly", "options")
+    declare readonly canOpenSelect: boolean;
+
+    @property({ type: String })
+    @computed(function(this: PersistentObjectAttributeReference, attribute: Vidyano.PersistentObjectAttributeWithReference): string {
+        return attribute && attribute.getTypeHint("orientation", "vertical");
+    }, "attribute")
+    declare readonly orientation: string;
+
+    @property({ type: String })
+    @computed(function(this: PersistentObjectAttributeReference, attribute: Vidyano.PersistentObjectAttribute, href: string): string {
+        return attribute && href && attribute.parent.isNew ? "_blank" : "";
+    }, "attribute", "href")
+    declare readonly target: string;
+
+    @property({ type: String })
+    @computed(function(this: PersistentObjectAttributeReference, displayValue: string, sensitive: boolean): string {
+        return !sensitive ? displayValue : "";
+    }, "attribute.displayValue", "sensitive")
+    declare readonly title: string;
+
+    @property({ type: String })
+    @computed(function(this: PersistentObjectAttributeReference, attribute: Vidyano.PersistentObjectAttributeWithReference, objectId: string): string {
+        if (!(attribute instanceof Vidyano.PersistentObjectAttributeWithReference))
+            return null;
+
+        if (this.app && 'getUrlForPersistentObject' in this.app && attribute.lookup && attribute.lookup.canRead && objectId)
+            return Path.routes.rootPath + (<any>this.app).getUrlForPersistentObject(attribute.lookup.persistentObject.id, objectId);
+
+        return null;
+    }, "attribute", "objectId")
+    declare readonly href: string;
+
+    @property({ type: Boolean })
+    @computed(function(this: PersistentObjectAttributeReference, attribute: Vidyano.PersistentObjectAttributeWithReference, editing: boolean, readOnly: boolean, sensitive: boolean, objectId: string): boolean {
+        if (!(attribute instanceof Vidyano.PersistentObjectAttributeWithReference))
+            return false;
+
+        return editing && !readOnly && !sensitive && !attribute.isRequired && !String.isNullOrEmpty(objectId);
+    }, "attribute", "editing", "readOnly", "sensitive", "objectId")
+    declare readonly canClear: boolean;
+
+    @property({ type: Boolean })
+    @computed(function(this: PersistentObjectAttributeReference, attribute: Vidyano.PersistentObjectAttributeWithReference, editing: boolean, readOnly: boolean, sensitive: boolean): boolean {
+        if (!(attribute instanceof Vidyano.PersistentObjectAttributeWithReference))
+            return false;
+
+        return editing && !readOnly && !sensitive && attribute.canAddNewReference;
+    }, "attribute", "editing", "readOnly", "sensitive")
+    declare readonly canAddNewReference: boolean;
+
+    @property({ type: Boolean })
+    @computed(function(this: PersistentObjectAttributeReference, attribute: Vidyano.PersistentObjectAttributeWithReference, editing: boolean, readOnly: boolean, sensitive: boolean): boolean {
+        if (!(attribute instanceof Vidyano.PersistentObjectAttributeWithReference))
+            return false;
+
+        return editing && !readOnly && !sensitive && !attribute.selectInPlace;
+    }, "attribute", "editing", "readOnly", "sensitive")
+    declare readonly canBrowseReference: boolean;
 
     protected _attributeChanged() {
         super._attributeChanged();
 
-        this._update();
+        if (this.attribute instanceof Vidyano.PersistentObjectAttributeWithReference)
+            this.filter = this.attribute.value;
+        else
+            this.filter = "";
     }
 
+    @observer("value")
     protected _valueChanged(newValue: any) {
-        this._update();
+        this.filter = newValue || "";
 
-        if (this.attribute && newValue !== this.attribute.value) {
+        if (this.attribute && newValue !== this.attribute.value)
             this.attribute.setValue(newValue, true).catch(Vidyano.noop);
-        }
-    }
-
-    private _objectIdChanged() {
-        if (this.attribute && this.attribute.objectId !== this.objectId)
-            this.attribute.changeReference(this.objectId ? [this.objectId] : []);
     }
 
     private async _filterBlur() {
@@ -106,10 +125,8 @@ export class PersistentObjectAttributeReference extends Polymer.PersistentObject
             if (!result)
                 return;
 
-            if (result.length === 1) {
+            if (result.length === 1)
                 await this.attribute.changeReference([result[0]]);
-                this._update();
-            }
             else {
                 if (result.length === 0) {
                     this.filter = this.attribute.value;
@@ -125,64 +142,31 @@ export class PersistentObjectAttributeReference extends Polymer.PersistentObject
             this.filter = this.attribute.value;
     }
 
-    protected _editingChanged() {
-        this._update();
-    }
-
-    private _browse(e: Polymer.Gestures.TapEvent) {
+    private _browse(_e: Event) {
         this.attribute.lookup.textSearch = "";
         this._browseReference(false, true);
     }
 
-    private async _browseReference(throwExceptions?: boolean, forceSearch?: boolean): Promise<any> {
+    private async _browseReference(_throwExceptions?: boolean, forceSearch?: boolean): Promise<any> {
         this.attribute.lookup.selectedItems = [];
 
-        try {
-            const result = await this.app.showDialog(new SelectReferenceDialog(this.attribute.lookup, forceSearch, this.canAddNewReference));
-            if (!result)
-                return;
-
-            if (result instanceof Array && result.length > 0 && result[0] instanceof Vidyano.QueryResultItem) {
-                await this.attribute.changeReference(result);
-                this._update();
-            }
-
-            if (result === "AddNewReference")
-                this._addNewReference();
-        }
-        finally {
-            this.filter = this.attribute.value;
-        }
-    }
-
-    private _addNewReference(e?: Event) {
-        this.attribute.addNewReference();
-    }
-
-    private async _clearReference(e: Event) {
-        await this.attribute.changeReference([]);
-        this._update();
-    }
-
-    private _update() {
-        if (!this.isConnected)
+        const result = await this.app.showDialog(new SelectReferenceDialog(this.attribute.lookup, forceSearch, this.canAddNewReference));
+        if (!result)
             return;
 
-        const hasReference = this.attribute instanceof Vidyano.PersistentObjectAttributeWithReference;
+        if (result instanceof Array && result.length > 0 && result[0] instanceof Vidyano.QueryResultItem)
+            await this.attribute.changeReference(result);
 
-        if (hasReference && this.attribute.objectId !== this.objectId)
-            this.objectId = this.attribute ? this.attribute.objectId : null;
+        if (result === "AddNewReference")
+            await this._addNewReference();
+    }
 
-        if (this.app && 'getUrlForPersistentObject' in this.app && hasReference && this.attribute.lookup && this.attribute.lookup.canRead && this.attribute.objectId)
-            this.href = Path.routes.rootPath + (<any>this.app).getUrlForPersistentObject(this.attribute.lookup.persistentObject.id, this.attribute.objectId);
-        else
-            this.href = null;
+    private async _addNewReference(_e?: Event) {
+        await this.attribute.addNewReference();
+    }
 
-        this.filter = hasReference ? this.attribute.value : "";
-
-        this._setCanClear(hasReference && this.attribute.parent.isEditing && !this.attribute.isReadOnly && !this.sensitive && !this.attribute.isRequired && !String.isNullOrEmpty(this.attribute.objectId));
-        this._setCanAddNewReference(hasReference && this.attribute.parent.isEditing && !this.attribute.isReadOnly && !this.sensitive && this.attribute.canAddNewReference);
-        this._setCanBrowseReference(hasReference && this.attribute.parent.isEditing && !this.attribute.isReadOnly && !this.sensitive && !this.attribute.selectInPlace);
+    private async _clearReference(_e: Event) {
+        await this.attribute.changeReference([]);
     }
 
     private _openSelect() {
@@ -206,31 +190,106 @@ export class PersistentObjectAttributeReference extends Polymer.PersistentObject
         }
     }
 
-    private _computeTarget(attribute: Vidyano.PersistentObjectAttribute, href: string): string {
-        return attribute && href && attribute.parent.isNew ? "_blank" : "";
+    protected renderDisplay() {
+        return html`
+            <a href=${this.href || nothing} title=${this.title || nothing} ?disabled=${!this.href} @click=${this._open} target=${this.target || nothing}>
+                <vi-sensitive ?disabled=${!this.sensitive}>
+                    <span>${this.attribute?.displayValue}</span>
+                </vi-sensitive>
+                ${this.href ? html`
+                    <vi-icon source="ArrowUpRight" class="size-h4"></vi-icon>
+                ` : nothing}
+            </a>
+        `;
     }
 
-    private _computeInputType(attribute: Vidyano.PersistentObjectAttributeWithReference): string {
-        return attribute && attribute.getTypeHint("inputtype", "default");
+    #renderEditSelectDefault() {
+        return super.renderEdit(html`
+            <vi-select id="selectInPlace" class="fit" .options=${this.options} .selectedOption=${this.objectId} @selected-option-changed=${(e: CustomEvent) => this.attribute.changeReference([e.detail.value])} ?readonly=${this.readOnly} placeholder=${this.placeholder || nothing} ?sensitive=${this.sensitive} ?disabled=${this.frozen}></vi-select>
+            <vi-button slot="right" @click=${this._openSelect} ?hidden=${!this.canOpenSelect} tabindex="-1" ?disabled=${this.frozen}>
+                <vi-icon source="CaretDown"></vi-icon>
+            </vi-button>
+            <a slot="right" href=${this.href || nothing} title=${this.title || nothing} ?disabled=${!this.href} tabindex="-1" @click=${this._open} target=${this.target || nothing}>
+                <vi-icon source="ArrowUpRight"></vi-icon>
+            </a>
+            ${this.canAddNewReference ? html`
+                <vi-button slot="right" @click=${this._addNewReference} tabindex="-1" ?disabled=${this.frozen}>
+                    <vi-icon source="Add"></vi-icon>
+                </vi-button>
+            ` : nothing}
+            ${this.canClear ? html`
+                <vi-button slot="right" @click=${this._clearReference} tabindex="-1" ?disabled=${this.frozen}>
+                    <vi-icon source="Remove"></vi-icon>
+                </vi-button>
+            ` : nothing}
+        `);
     }
 
-    private _computeOrientation(attribute: Vidyano.PersistentObjectAttributeWithReference): string {
-        return attribute && attribute.getTypeHint("orientation", "vertical");
+    #renderEditSelectRadio() {
+        return html`
+            <div>
+                <div id="radiobuttons" orientation=${this.orientation || nothing}>
+                    ${this.options?.map((option: any) => html`
+                        <vi-checkbox label=${option.value} ?checked=${option.key === this.objectId} @changed=${(e: CustomEvent) => { e.stopPropagation(); this.attribute.changeReference([option.key]); }} radio part="radio"></vi-checkbox>
+                    `)}
+                </div>
+            </div>
+        `;
     }
 
-    private _computeCanOpenSelect(isReadOnly: boolean, options: string[]): boolean {
-        return !isReadOnly && !!options && options.length > 0;
+    #renderEditSelectChip() {
+        return html`
+            <div>
+                <div id="chips" orientation=${this.orientation || nothing}>
+                    ${this.options?.map((option: any) => html`
+                        <vi-button label=${option.value} ?inverse=${option.key !== this.objectId} @click=${(e: Event) => { e.stopPropagation(); this.attribute.changeReference([option.key]); }} part="chip"></vi-button>
+                    `)}
+                </div>
+            </div>
+        `;
     }
 
-    private _computeTitle(displayValue: string, sensitive: boolean): string {
-        return !sensitive ? displayValue : "";
+    #renderEditInput() {
+        return super.renderEdit(html`
+            <vi-sensitive ?disabled=${!this.sensitive}>
+                <input .value=${this.filter || ""} @input=${(e: InputEvent) => this.filter = (e.target as HTMLInputElement).value} @blur=${this._filterBlur} ?readonly=${this.readOnly} tabindex=${this.readOnlyTabIndex || nothing} placeholder=${this.placeholder || nothing} ?disabled=${this.frozen}>
+            </vi-sensitive>
+            <a slot="right" href=${this.href || nothing} title=${this.title || nothing} ?disabled=${!this.href} tabindex="-1" @click=${this._open} target=${this.target || nothing}>
+                <vi-icon source="ArrowUpRight"></vi-icon>
+            </a>
+            ${this.canBrowseReference ? html`
+                <vi-button slot="right" @click=${this._browse} tabindex="-1" ?disabled=${this.frozen}>
+                    <vi-icon source="Ellipsis"></vi-icon>
+                </vi-button>
+            ` : nothing}
+            ${this.canAddNewReference ? html`
+                <vi-button slot="right" @click=${this._addNewReference} tabindex="-1" ?disabled=${this.frozen}>
+                    <vi-icon source="Add"></vi-icon>
+                </vi-button>
+            ` : nothing}
+            ${this.canClear ? html`
+                <vi-button slot="right" @click=${this._clearReference} tabindex="-1" ?disabled=${this.frozen}>
+                    <vi-icon source="Remove"></vi-icon>
+                </vi-button>
+            ` : nothing}
+        `);
     }
 
-    private _select(e: CustomEvent) {
-        e.stopPropagation();
+    protected renderEdit() {
+        if (this.attribute?.selectInPlace) {
+            if (this.inputtype === "default")
+                return this.#renderEditSelectDefault();
 
-        this.objectId = (<any>e).model.option.key;
+            if (this.inputtype === "radio")
+                return this.#renderEditSelectRadio();
+
+            if (this.inputtype === "chip")
+                return this.#renderEditSelectChip();
+        }
+
+        return this.#renderEditInput();
     }
 }
 
+customElements.define("vi-persistent-object-attribute-reference", PersistentObjectAttributeReference);
 PersistentObjectAttributeRegister.add("Reference", PersistentObjectAttributeReference);
