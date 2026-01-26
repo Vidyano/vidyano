0c2e1ad37b1218f09a072040ebb0b38eb5c4ce79
David Sleeckx <david@sleeckx.be>
2026-01-26 13:22:47 +0000

    chore: Update devcontainer and CI for monorepo and local act testing

--- packages/core/rollup.config.mjs
+++ packages/core/rollup.config.mjs
@@ -3,7 +3,7 @@ const require = createRequire(import.meta.url);
 const pjson = require('./package.json');
 
 import nodeResolve from '@rollup/plugin-node-resolve';
-import vulcanize from '../../rollup.vulcanize.js';
+import vulcanize from '../vidyano/rollup.vulcanize.js';
 import { dts } from "rollup-plugin-dts";
 import replace from "@rollup/plugin-replace";
 import terser from '@rollup/plugin-terser';
